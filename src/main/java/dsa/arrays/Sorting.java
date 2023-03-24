cc225c947ce7ea7ce02b1f4e2ec5bb69b61fbf7f
divya.jnu08 <divya.srivastava@impactsure.com>
2023-03-24 07:58:45 +0530

    sorting algorithms on array

--- src/main/java/dsa/arrays/Sorting.java
+++ src/main/java/dsa/arrays/Sorting.java
@@ -59,7 +59,81 @@ public class Sorting {
 			arr[j+1]=key;
 		}
 	}
-
+	
+	public static void mergeSort(int arr[],int l,int r) {
+		
+		if(l<r) {
+			int m=l+(r-l)/2;
+			mergeSort(arr,l,m);
+			mergeSort(arr,m+1,r);
+			merge(arr,l,m,r);
+		}
+	}
+	
+	public static void merge(int[] arr,int l,int m,int r) {
+		int n1=m-l+1;
+		int n2=r-m;
+		
+		int L[]=new int[n1];
+		int R[]=new int[n2];
+		
+		for(int i=0;i<n1;i++) {
+			L[i]=arr[l+i];
+		}
+		
+		for(int j=0;j<n2;j++) {
+			R[j]=arr[m+j+1];
+		}
+		
+		int i=0,j=0,k=l;
+		while(i < n1 && j < n2 ) {
+			if(L[i]<=R[j]) {
+				arr[k++]=L[i++];
+			}else{
+				arr[k++]=R[j++];
+			}
+		}
+		
+		while(i<n1) {
+			arr[k++]=L[i++];
+		}
+		
+		while(j<n2) {
+			arr[k++]=R[j++];
+		}
+		
+	}
+	
+	public static void swap(int[] arr, int i,int j) {
+		int temp=arr[i];
+		arr[i]=arr[j];
+		arr[j]=temp;
+	}
+	
+	public static int partition(int[] arr,int low,int high) {
+		int pivot=arr[high];
+		int i = low-1;
+		for(int j=0;j<=high-1;j++) {
+			if(arr[j]<pivot) {
+				i++;
+				swap(arr,i,j);
+			}
+		}
+		swap(arr,i+1,high);
+		return i+1;
+		
+	}
+	
+	public static void quickSort(int[] arr,int low,int high) {
+		if(low<high) {
+			int mid=partition(arr,low,high);
+			quickSort(arr,low,mid-1);
+			quickSort(arr,mid+1,high);
+		}
+	}
+	
+	
+	
 	public static void printArray(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			System.out.print(arr[i] + " ");
@@ -76,6 +150,10 @@ public class Sorting {
 		recurBubbleSort(arr, arr.length);
 		printArray(arr);
 		insertionSort(arr);
+		printArray(arr);
+		mergeSort(arr,0,arr.length-1);
+		printArray(arr);
+		quickSort(arr,0,arr.length-1);
 		printArray(arr);
 	}
 
