baa90b8558c4fdf6e11eab06116d098f1406935c
divya.jnu08 <divya.srivastava@impactsure.com>
2023-03-25 08:23:59 +0530

    sorting algorithms

--- src/main/java/dsa/arrays/Sorting.java
+++ src/main/java/dsa/arrays/Sorting.java
@@ -140,6 +140,42 @@ public class Sorting {
 		}
 		System.out.println();
 	}
+	
+	public static void heapify(int arr[],int i,int n) {
+		int l=2*i+1;
+		int r=2*i+2;
+		
+		int largest=i;
+		
+		if(l<n && arr[l]>arr[largest]) {
+			largest=l;
+		}
+		
+		if(r<n && arr[r]>arr[largest]) {
+			largest=r;
+		}
+		
+		if(largest!=i) {
+			int temp=arr[i];
+			arr[i]=arr[largest];
+			arr[largest]=temp;
+			heapify(arr,largest,n);
+		}
+		
+	}
+	
+	public static void heapSort(int[] arr,int n) {
+		for(int i=n/2-1;i>=0;i--) {
+			heapify(arr,i,n);
+		}
+		
+		for(int i=n-1;i>=0;i--) {
+			int temp=arr[0];
+			arr[0]=arr[i];
+			arr[i]=temp;
+			heapify(arr,i,0);
+		}
+	}
 
 	public static void main(String args[]) {
 		int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
@@ -154,6 +190,8 @@ public class Sorting {
 		mergeSort(arr,0,arr.length-1);
 		printArray(arr);
 		quickSort(arr,0,arr.length-1);
+		printArray(arr);
+		heapSort(arr,arr.length-1);
 		printArray(arr);
 	}
 
